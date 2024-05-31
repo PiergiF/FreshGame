cd1ba689d67a89c6bc0b37b451b95f3f0792aa4c
PiergiF <pieegi02@gmail.com>
2024-05-31 11:39:07 +0200

    start project

---
+++ src/main/java/com/uniroma3/siw/freshgame/FreshgameApplication.java
@@ -0,0 +1,13 @@
+package com.uniroma3.siw.freshgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshgameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniroma3/siw/freshgame/FreshgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniroma3.siw.freshgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
